40594ec401134332d5ee1660bb0cbccb081af452
宋冰琦 <wy19939334056@163.com>
2022-12-19 17:30:19 +0800

    123

--- src/plugins/message.js
+++ src/plugins/message.js
@@ -18,16 +18,27 @@ function saveImage(url,path) {
         req.setEncoding('binary');
         req.on('end',function () {
             fs.writeFile(path,imgData,'binary',function (err) {
-                console.log(err)
                 console.log('保存图片成功'+path)
             })
         })
     })
 }
+//判断文件是否存在
+function judgeFileExist(fileUrl) {
+    return new Promise((resolve, reject) => {
+        fs.access(fileUrl, fs.constants.F_OK, (err) => {
+          if (err) { // 文件存在时err = false；文件不存在时err = true
+            resolve(false);
+          } else {
+            resolve(true);
+          }
+        });
+    })
+  }
 
 function OnMessageReceive(msg){
     console.log(msg)
-    const {user_id, atme, message, friend, member, group, message_type} = msg
+    const {user_id, atme, message, friend = {}, member = {}, group = {}, message_type} = msg
     const mts = message.filter(e=>{return e.type==='text'})[0].text.trim();
     const atList = message.filter(e=>{return e.type==='at'&&e.qq!=647666737});
     const fileList = message.filter(e=>{return e.type==='image'})
@@ -52,21 +63,26 @@ function OnMessageReceive(msg){
                 }
                 break;
             case '查证':
-                const message = [
-                    "hello world",
-                    segment.image(path.resolve(__dirname, '../../') + '/src/personPic/2272578756.jpg'),
-                    segment.face(104),
-                    segment.at(10001),
-                ]
-                msg.reply(message, true);
+                const picPath = path.resolve(__dirname, '../../') + `/src/personPic/${atList[0].qq}.jpg`
+                judgeFileExist(picPath).then(err=>{
+                    console.log(err ? '文件存在' : '文件不存在')
+                    if(err) {
+                        const message = [
+                            "完成",
+                            segment.face(104),
+                            segment.image(picPath),
+                            segment.at(10001),
+                        ]
+                        msg.reply(message, true);
+                    }else{
+                        msg.reply(atList[0].text+'用户未认证', true);
+                    }
+                })
+                break;
         }
     } else {
         if(message_type === 'private') {
             if(mts === '点赞') {
-                friend.thumbUp()
-                friend.thumbUp()
-                friend.thumbUp()
-                friend.thumbUp()
                 friend.thumbUp()
                 msg.reply('完成', true);
             }
